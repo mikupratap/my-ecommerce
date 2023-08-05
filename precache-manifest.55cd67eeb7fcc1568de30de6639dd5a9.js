0cfa1b4b4ecf4ae2c9135b220874594ab997032d
mikupratap <nkmkbnkmkb@gmail.com>
2023-08-05 22:16:08 +0530

    Updates

---
+++ precache-manifest.55cd67eeb7fcc1568de30de6639dd5a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf2b95eec903be4c4e5a669566b236b",
+    "url": "/my-ecommerce/index.html"
+  },
+  {
+    "revision": "9f034d6e5229e97ec392",
+    "url": "/my-ecommerce/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "666adfe5779aaaac18e0",
+    "url": "/my-ecommerce/static/css/main.32bbb75a.chunk.css"
+  },
+  {
+    "revision": "9f034d6e5229e97ec392",
+    "url": "/my-ecommerce/static/js/2.58ffb7c9.chunk.js"
+  },
+  {
+    "revision": "666adfe5779aaaac18e0",
+    "url": "/my-ecommerce/static/js/main.2d99f0e1.chunk.js"
+  },
+  {
+    "revision": "c07f84d25d517a8f7ac2",
+    "url": "/my-ecommerce/static/js/runtime~main.5853b4f0.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/my-ecommerce/static/media/logo.9f7e506d.svg"
+  }
+]);
